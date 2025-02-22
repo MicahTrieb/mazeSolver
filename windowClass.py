97996243f6bbce74759a3a32b468c06a79fb2726
lowrezdragon <lowrezdragon@gmail.com>
2025-02-22 16:25:40 -0600

    Building out window class

--- windowClass.py
+++ windowClass.py
@@ -4,3 +4,8 @@ class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.__root = Tk()
+        self.__root.title = "Title"
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.running = False
