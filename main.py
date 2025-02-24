9cf050b8e872bc6ccf9f237173ddac5d128033bb
lowrezdragon <lowrezdragon@gmail.com>
2025-02-24 14:58:20 -0600

    Buliding cell class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from windowClass import *
 if __name__ == "__main__":
     def main():
         win = Window(800, 600)
-        Window.draw_line(Line(Point(5,5), Point(250,250)))
+        win.draw_line(Line(Point(5,5), Point(250,250)), "black")
         win.wait_for_close()
 
 main()
