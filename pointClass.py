3f2d7dcc6e7fe53dfaa3811061006ea0e21645aa
lowrezdragon <lowrezdragon@gmail.com>
2025-02-24 13:04:02 -0600

    Building more classes, got functionality without issues

---
+++ pointClass.py
@@ -0,0 +1,16 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, color):
+        Canvas.create_line(
+            self.point1.x
+        )
