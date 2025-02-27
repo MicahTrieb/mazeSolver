a2fb4085b6ee347151064841986ded88226e858d
lowrezdragon <lowrezdragon@gmail.com>
2025-02-27 14:36:41 -0600

    figuring it out!

--- mazeClass.py
+++ mazeClass.py
@@ -1,41 +1,24 @@
-from classes import *
-import time
+from windowClasses import *
+from cellClass import *
+
 class Maze:
-    def __init__(
-        self,
-        x1,
-        y1,
-        num_rows,
-        num_cols,
-        cell_size_x,
-        cell_size_y,
-        win=None,
-    ):
+    def __init__(self,rows, cols, xSize, ySize, x1=0, y1=0, win=None):
         self._x1 = x1
         self._y1 = y1
-        self._num_rows = num_rows
-        self._num_cols = num_cols
-        self._cell_size_x = cell_size_x
-        self._cell_size_y = cell_size_y
-        self._win = win
-        self._cells = []
-        
+        self.rows = rows
+        self.cols = cols
+        self.xSize = xSize
+        self.ySize = ySize
+        self.win = win
+
+        self._create_cells()
     def _create_cells(self):
-        for i in range(self._num_cols):
-            col_cells = []
-            for j in range(self._num_rows):
-                col_cells.append(Cell(self._win))
-            self._cells.append(col_cells)
-        for i in range(self._num_cols):
-            for j in range(self._num_rows):
+        for i in range(self.cols):
+            for j in range(self.rows):
                 self._draw_cell(i, j)
-    def _draw_cell(self, row, col):
-        x1 = self._x1 + col * self._cell_size_x
-        y1 = self._y1 + row * self._cell_size_y
-        x2 = x1 + self._cell_size_x
-        y2 = y1 + self._cell_size_y
-        self._cells[col][row].draw(x1, y1, x2, y2)
-        self._animate()
-    def _animate(self):
-        self.redraw()
-        time.sleep(0.05)
+
+    def _draw_cells(self, i, j):
+        topLeft = Point(self._x1 + (i * self.xSize), self.y1 + (i * self.ySize))
+        topRight = Point(self._x1 + ((i + 1) * self.xSize), self.y1 + (i * self.ySize) )
+        bottomLeft = Point(self._x1 + (i * self.xSize), self._y1)
+        bottomRight = Point(self._x1 + ((i + 1) * self.xSize),)
