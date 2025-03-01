7765ace0808c9b5eec1ccc5d76debcf1be9fe3de
lowrezdragon <lowrezdragon@gmail.com>
2025-02-28 22:26:00 -0600

    THE MAZE DRAWS

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from windowClasses import *
 from cellClass import *
+from mazeClass import Maze
 def main():
     window = Window(800,600)
-    window.draw_line(Line(Point(5,5),Point(250,250)), "pink")
-    newCell = Cell(25,100,40,160, window)
-    newCell.draw(newCell.x1,newCell.y1,newCell.x2, newCell.y2, window._canvas, "red")
-    newCell2 = Cell(250, 350, 290, 380, window)
-    newCell2.draw(newCell2.x1, newCell2.y1, newCell2.x2, newCell2.y2, window._canvas, "blue")
-    newCell.draw_move(newCell2)
+    #window.draw_line(Line(Point(5,5),Point(250,250)), "pink")
+    newMaze = Maze(10, 10, 10, 10, 0, 0, window)
+    #newMaze._create_cells()
     window.wait_for_close() 
 
 
--- mazeClass.py
+++ mazeClass.py
@@ -1,6 +1,6 @@
 from windowClasses import *
 from cellClass import *
-
+from time import time
 class Maze:
     def __init__(self,rows, cols, xSize, ySize, x1=0, y1=0, win=None):
         self._x1 = x1
@@ -15,10 +15,17 @@ class Maze:
     def _create_cells(self):
         for i in range(self.cols):
             for j in range(self.rows):
-                self._draw_cell(i, j)
+                self._draw_cells(i, j)
 
     def _draw_cells(self, i, j):
-        topLeft = Point(self._x1 + (i * self.xSize), self.y1 + (i * self.ySize))
-        topRight = Point(self._x1 + ((i + 1) * self.xSize), self.y1 + (i * self.ySize) )
-        bottomLeft = Point(self._x1 + (i * self.xSize), self._y1)
-        bottomRight = Point(self._x1 + ((i + 1) * self.xSize),)
+        topLeft = self._x1 + (i * self.xSize)
+        topRight = self._y1 + (j * self.ySize)
+        bottomLeft = self._x1 + (i * self.xSize)
+        bottomRight = self._y1 + ((j + 1) * self.ySize)
+        newCell = Cell(topLeft, topRight, bottomLeft, bottomRight, self.win)
+        newCell.draw(newCell.x1, newCell.y1, newCell.x2, newCell.y2, self.win._canvas, "purple")
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        #time.sleep(0.05)
